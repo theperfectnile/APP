103835e9d1fcb70898be706c69207143353946b6
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-10 20:34:00 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -131,4 +131,4 @@ app.post('/api/upgrade', auth, (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Portfolio Analyzer running on port 4000`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Portfolio Analyzer running on port ${PORT}`));
