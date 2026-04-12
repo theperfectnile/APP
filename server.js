77ecc11a93ce401bc820d50d251148a62c3adf66
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-11 23:58:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,13 @@ app.use(
 app.use(express.json());
 
 const JWT_SECRET = process.env.JWT_SECRET;
-
+function requireApiKey(req, res, next) {
+  const key = req.headers["x-api-key"];
+  if (!key || key !== process.env.API_KEY) {
+    return res.status(403).json({ error: "Forbidden: Invalid API Key" });
+  }
+  next();
+}
 /* ------------------ MONGODB CONNECTION ------------------ */
 
 mongoose
