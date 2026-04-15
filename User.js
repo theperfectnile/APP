ab73cd98fd02a9b9ceaf061030fa6791ecca52f6
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-15 16:42:58 -0700

    Update User.js

--- User.js
+++ User.js
@@ -1,22 +1,28 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     email: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
-      trim: true
     },
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
+
+    // Trial fields
+    trialStart: {
+      type: Date,
+      default: null,
+    },
+    trialActive: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
- trialStart: { type: Date, default: null },
-  trialActive: { type: Boolean, default: false },
-});
-module.exports = mongoose.model("User", UserSchema);
+
+module.exports = mongoose.model("User", userSchema);
