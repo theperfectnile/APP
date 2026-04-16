41f2baa647389721210b82077e3181cd07767bab
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-16 16:55:57 -0700

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./models/User");
+const { register, login } = require("./controllers/authController");
 
-const { register, login } = require("./authController.js");
-
-// Register
 router.post("/register", register);
-
-// Login
 router.post("/login", login);
 
 module.exports = router;
