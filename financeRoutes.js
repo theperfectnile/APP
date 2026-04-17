aa9eb64239e62f6d921209d03641cd5ba9476e14
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-16 17:31:49 -0700

    Update financeRoutes.js

--- financeRoutes.js
+++ financeRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+
+// Correct folder structure imports
 const auth = require("./middleware/authMiddleware");
 const {
   addEntry,
@@ -8,6 +10,7 @@ const {
   analyze
 } = require("./controllers/financeController");
 
+// Routes
 router.post("/add", auth, addEntry);
 router.get("/all", auth, getAll);
 router.get("/summary", auth, summary);
